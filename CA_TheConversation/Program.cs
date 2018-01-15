c9f70a1c31227b1ba5bd532b9d341b40efc843a4
John Velis <john@johnvelis.com>
2018-01-15 09:45:23 -0500

    initial push

--- CA_TheConversation/Program.cs
+++ CA_TheConversation/Program.cs
@@ -8,8 +8,33 @@ namespace CA_TheConversation
 {
     class Program
     {
+        // *************************************************************
+        // Application:     The Conversation Starter Solution
+        // Author:          Velis, John E
+        // Description:     Start solution for the project.
+        // Date Created:    5/20/2016
+        // Date Revised:    7/7/2016
+        // *************************************************************
+
         static void Main(string[] args)
         {
+            //
+            // display a welcome to the user
+            //
+            Console.WriteLine("Greetings! Welcome to our conversation.");
+            Console.WriteLine();
+
+            //
+            // begin your code
+            //
+
+
+            //
+            // display a prompt to exit the application
+            //
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the application.");
+            Console.ReadKey();
         }
     }
 }
